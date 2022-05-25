d573df7f9e854174a9ed2200e2bbf901dd337953
LucasPerezDev <LucasPerezDev@gmail.com>
2022-05-24 21:02:37 -0400

    added to the project details section

--- src/components/ProjectsDetailsSection.js
+++ src/components/ProjectsDetailsSection.js
@@ -1,33 +1,48 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
 function ProjectsDetailsSection(props) {
-
-
-    return(
-        <div>
-            <Card style={{ width: '10rem', height: '3rem', justifyContent: 'center', marginBottom: '25px', marginTop: '50px' }}>
+  return (
+    <div>
+      <Card
+        style={{
+          width: "10rem",
+          height: "3rem",
+          justifyContent: "center",
+          marginBottom: "25px",
+          marginTop: "50px",
+        }}
+      >
+        <Card.Body>
+          <Card.Title>Projects</Card.Title>
+        </Card.Body>
+      </Card>
+      {props.user.project &&
+        props.user.project.map((project) => {
+          return (
+            <>
+              <Card
+                className="container"
+                style={{
+                  height: "4rem",
+                  justifyContent: "center",
+                  marginBottom: "25px",
+                }}
+              >
                 <Card.Body>
-                    <Card.Title>Projects</Card.Title>
+                  <Card.Text>
+                    <h6>{project.description}</h6>
+                    <p>{project.projectLink}</p>
+                    <p>{project.features}</p>
+                    <p>{project.madeWith}</p>
+                  </Card.Text>
                 </Card.Body>
-            </Card>
-            { props.user.project && props.user.project.map((project) => {
-            return (
-                <>
-                <Card className="container" style={{ height: '4rem', justifyContent: 'center', marginBottom: '25px'}}>
-                    <Card.Body>
-                        <Card.Text>
-                        <h6>{project.description}</h6>
-                                {/* project.madeWith, etc */}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                </>
-            )
-            } )
-            }
-        </div>
-    )
+              </Card>
+            </>
+          );
+        })}
+    </div>
+  );
 }
 
 export default ProjectsDetailsSection;
